ef09eca3d0befbcd053fa19e746f4bbf565fcf33
AntoniRL <antonio.radomiski@gmail.com>
2025-05-09 19:51:17 +0200

    interpreter.py

--- src/agentar.py
+++ src/agentar.py
@@ -1,24 +1,7 @@
 import sys
 from antlr4 import *
 
-from antlr.AgentarLexer import AgentarLexer
-from antlr.AgentarParser import AgentarParser
-from antlr.AgentarVisitor import AgentarVisitor
-
-class AgentarInterpreter(AgentarVisitor):
-    def visitPrintStmt(self, ctx):
-        text = ctx.STRING().getText()  # Pobiera tekst w cudzysłowach
-        print(text[1:-1])  # Obcina cudzysłowy
-
-def run_agentar(file_path):
-    input_stream = FileStream(file_path)
-    lexer = AgentarLexer(input_stream)
-    stream = CommonTokenStream(lexer)
-    parser = AgentarParser(stream)
-    tree = parser.program()
-    
-    interpreter = AgentarInterpreter()
-    interpreter.visit(tree)
+from compiler.interpreter import run_agentar
 
 def main():
     if len(sys.argv) < 2:
--- src/compiler/agenter_builtins.py
+++ src/compiler/agenter_builtins.py
@@ -1,3 +1,4 @@
 # Wbudowane funkcje języka Agentar
 # -*- coding: utf-8 -*-
 
+print("Wczytywanie wbudowanych funkcji języka Agentar...")
