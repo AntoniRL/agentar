24099b4267174fafdf17083ca3f4ed904e36b18d
AntoniRL <antonio.radomiski@gmail.com>
2025-11-25 16:33:19 +0100

    scenario

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
         "antlr4-python3-runtime>=4.9.3",
         "rich>=13.0.0",
         "numpy>=1.21.0",
-        "antlr4-tools>=4.9.3",
+        "antlr4-tools==0.2.2",
+        "psutil>=5.8.0",
     ],
     entry_points={
         "console_scripts": [
